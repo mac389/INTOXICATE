95f0454371c6044a0a0860ecd4ff8d2c99b308f9
rzemla <rzemla@gmail.com>
2024-04-07 19:26:10 -0400

    create matrixes for generating INTOXICATE score

--- src/for_model/create-synthetic-database.py
+++ src/for_model/create-synthetic-database.py
@@ -42,6 +42,20 @@ def create_patient():
     ans['risk'] = risk_score
     return ans
 
+def calculate_risk_score(patient):
+    risk_score = 0
+    for variable_name in predictive_variables:
+        score = patient[f'{variable_name}_value']
+        kernel_variable_name = slugify(variable_name.lower(), separator='_')
+        if kernel_variable_name == 'CO, As, CN':
+            weight = flattened_kernel['toxins_nos']
+        elif kernel_variable_name == 'intoxicant':
+            weight = flattened_kernel[slugify(patient['intoxicant_name'].lower(), separator='_')]
+        else:
+            weight = flattened_kernel[kernel_variable_name]
+        risk_score += (score * weight)
+    return risk_score
+
 
 input_ranges = []
 #create list of lists for each possible values for each input class
@@ -58,9 +72,29 @@ for index,val in enumerate(input_ranges[0]):
     all_values.append(np.array(np.meshgrid(val,*input_ranges[1:])).T.reshape(-1,9))
 
 
-#Generate list of numpy vectors with weights for matrix contained each weight for diffent intoxicant
+#Generate list of numpy vectors with weights for matrix containing each weight for diffent intoxicant
+# order: intoxicant, 'age', 'sbp', 'hr', 'gcs', 'second_diagnose', 'cirrhosis', 'dysrhythmia', 'respiratory'
+#extract fixed betas (age --> resp in order)
+fixed_betas = []
+for sample in kernel[8:-1]:
+    fixed_betas.append(sample['variable_value'])
 
+toxin_betas = []
+for sample in kernel[1:8]:
+    toxin_betas.append(sample['variable_value'])
 
+#generate numpy array with each vector asssociated with each toxin
+#convert vectors to numpy arrays 
+fixed_betas = np.array(fixed_betas)
+toxin_betas = np.array(toxin_betas)
+
+#create matrix with repeat fixed betas for each different beta toxin
+x = np.tile(fixed_betas, (len(toxin_betas),1))
+#merge each individual toxin beta with each vector for fixed individual betas
+y = np.hstack((np.atleast_2d(toxin_betas).T, x))
+
+    #tox_fixed_betas = list(sample) + fixed_betas
+    
 #calculate INTOXICATE score for each class of toxin 
 
 all_values_np = np.vstack(all_values)
@@ -74,21 +108,6 @@ def create_each_possible_input():
     for variable_name in predictive_variables:
         data = yaml.safe_load(open(os.path.join(DATA_PATH, f'{variable_name}_values.yml'), 'r'))
 '''
-
-def calculate_risk_score(patient):
-    risk_score = 0
-    for variable_name in predictive_variables:
-        score = patient[f'{variable_name}_value']
-        kernel_variable_name = slugify(variable_name.lower(), separator='_')
-        if kernel_variable_name == 'CO, As, CN':
-            weight = flattened_kernel['toxins_nos']
-        elif kernel_variable_name == 'intoxicant':
-            weight = flattened_kernel[slugify(patient['intoxicant_name'].lower(), separator='_')]
-        else:
-            weight = flattened_kernel[kernel_variable_name]
-        risk_score += (score * weight)
-    return risk_score
-
 
 def split_patient(patient):
     variable_names = {key: value['name'] for key, value in patient.items()}
