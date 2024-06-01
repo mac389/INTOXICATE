972998e4112f37fe3079db2c3cf5a0eb95dcb8fb
Michael Chary <mac389@gmail.com>
2024-06-01 14:01:16 -0400

    update

--- src/for_model/create_synthetic_patient.py
+++ src/for_model/create_synthetic_patient.py
@@ -1,9 +1,9 @@
-import random
 import os
+import random
 import uuid
 
-from yaml import safe_load
 from slugify import slugify
+from yaml import safe_load
 
 '''
 1. Don't use absolute paths. They don't transfer well between systems. Use relative paths.
@@ -17,6 +17,8 @@ DATA_PATH = os.path.join(project_path, 'data', 'for_model')
 PATH_TO_PREDICTIVE_VARIABLES = os.path.join(DATA_PATH, 'predictive_variables.yml')
 predictive_variables = safe_load(open(PATH_TO_PREDICTIVE_VARIABLES, 'r'))
 
+variable_numeric_range = os.path.join(DATA_PATH, 'variable_numeric_range.yml')
+
 score = {variable['value']: {value['name']: value['value']
          for value in safe_load(open(os.path.join(DATA_PATH, f"{variable['value']}_values.yml"), 'r'))}
          for variable in predictive_variables}
@@ -27,7 +29,10 @@ def create_patient():
     for variable in predictive_variables:
         variable_name = variable['value']
 
+        value_numeric = random.int(variable_range[variable_name]['min'],
+                                   variable_name[variable_name]['max'])
         value_name, value_score = random.choice(list(score[variable_name].items()))
+
 
         patient[variable_name] = {}
         patient[variable_name]['value'] = value_name
