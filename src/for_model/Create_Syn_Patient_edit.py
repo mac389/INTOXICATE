b08fdf29ea67868d0c600f97a4a73b4b5afbfd58
Michael Chary <mac389@gmail.com>
2025-08-02 06:51:22 -0400

    partial update
    Abstract data from sim logic

--- src/for_model/Create_Syn_Patient_edit.py
+++ src/for_model/Create_Syn_Patient_edit.py
@@ -2,12 +2,11 @@ import random
 import os
 import uuid
 import numpy as np
-
+from scipy.stats import truncnorm
 
 from yaml import safe_load
 
-project_path = os.path.join('..', '..')
-DATA_PATH = os.path.join(project_path, 'data', 'for_model')
+DATA_PATH = os.path.join('..','..', 'data', 'for_model')
 
 PATH_TO_PREDICTIVE_VARIABLES = os.path.join(DATA_PATH, 'predictive_variables.yml')
 predictive_variables = safe_load(open(PATH_TO_PREDICTIVE_VARIABLES, 'r'))
@@ -18,16 +17,48 @@ name_to_score = {variable['value']: safe_load(open(os.path.join(DATA_PATH,
 
 allowable_range = {variable['value']: variable['allowed_values'] for variable in predictive_variables}
 
+PATH_TO_SIMULATION_VARIABLES = os.path.join(DATA_PATH, 'simulation_variables.yml')
+simulation_variables = safe_load(open(PATH_TO_SIMULATION_VARIABLES, 'r'))
+
+print("Loaded simulation variables:", simulation_variables)
+def simulate_patient_value_i(variable_name):
+    var = [item for item in simulation_variables if item['name'] == variable_name]
+    if len(var) == 0:
+        raise ValueError(f"Variable {variable_name} not found in simulation variables")
+    else:
+        var = var[0]
+
+    if var['type'] == 'categorical':
+        if 'values' in var:
+            if isinstance(var['values'][0], int):
+                return random.choice(var['values'])
+            elif isinstance(var['values'][0], dict):
+                options, probs = zip(*[(item['value'], item['probability']) for item in var['values']])
+                return np.random.choice(options, p=probs)
+        else:
+            return random.choice(allowable_range[variable_name])
+    elif var['type'] == 'continuous':
+        if var['distribution']['type'] == 'normal':
+            mean = var['distribution']['mean']
+            std_dev = var['distribution']['stddev']
+            lower_bound = allowable_range[var['name']]['min']
+            upper_bound = allowable_range[var['name']]['max']
+            return int(truncnorm.rvs(
+                (lower_bound - mean) / std_dev,
+                (upper_bound - mean) / std_dev,
+                loc=mean,
+                scale=std_dev
+            ))
 
 def simulate_patient_value(variable_name):
     if variable_name == 'intoxicant':
         return random.choice(allowable_range[variable_name])
     elif variable_name == 'age':
-        return np.round(np.random.normal(35, 20)) #based on average age of pt w/ overdoses
+        return int(np.random.normal(35, 20)) #based on average age of pt w/ overdoses
     elif variable_name == 'sbp':
-        return np.round(np.random.normal(120, 20)) #literature says it depends on intoxicant, hard to get an average for all intoxicants and is dose dependent. 
+        return int(np.random.normal(120, 20)) #literature says it depends on intoxicant, hard to get an average for all intoxicants and is dose dependent. 
     elif variable_name == 'hr':
-        return np.round(np.random.normal(90, 20)) #literature says it depends on intoxicant - look at intoxicate score (what do you want its av to be)
+        return int(np.random.normal(90, 20)) #depends on intoxicant - look at intoxicate score (what do you want its av to be)
     elif variable_name == 'gcs':
         return int(np.clip(np.round(np.random.normal(13, 5)), 1, 15)) #find literature/ depends on intoxicant
     elif variable_name == 'cirrhosis':
@@ -69,7 +100,9 @@ def create_patient():
     for variable in predictive_variables:
         name = variable['value']
         value = simulate_patient_value(name)
+        value_i = simulate_patient_value_i(name)
         score = score_from_value(name, value)
+        print(f"{name}: {value} (simulated: {value_i})")
         patient['presentation'] += [{'name': name, 'score': score, 'value': value}]
 
     patient['risk'] = sum([feature['score'] for feature in patient['presentation']])
